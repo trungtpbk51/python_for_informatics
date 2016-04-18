2d8879caeab515ad549e6039b20b876a59c8a3a0
nguyen-viet-thanh-trung <viet-thanh-trung.nguyen@telecom-paristech.fr>
2016-04-18 15:50:56 +0200

    exercises chapter4: rewrite program from previous chapter using function

--- exercise_32.py
+++ exercise_32.py
@@ -1,12 +1,15 @@
+def computepay(hours, rate):
+    if hours > 40:
+        pay = 40*rate + (hours-40)*rate*1.5
+    else:
+        pay = hours*rate
+    return pay
+    
 try:
     hours = input('Enter Hours: ')
     h = int(hours)
     rate = input('Enter Rate: ')
     r = float(rate)
-    if h > 40:
-        pay = 40*r + (h-40)*r*1.5
-    else:
-        pay = h*r
-    print 'Pay: ' + str(pay)
+    print 'Pay: ', computepay(h, r)
 except:
     print 'Error, please enter numeric input'
--- exercise_33.py
+++ exercise_33.py
@@ -1,17 +1,19 @@
+def computegrade(score):
+    if s < 0.0 or s > 1.0:
+        return 'Bad score'
+    elif s >= 0.9:
+        return 'A'
+    elif s >= 0.8:
+        return 'B'
+    elif s >= 0.7:
+        return 'C'
+    elif s >= 0.6:
+        return 'D'
+    else:
+        return 'F'
 try:
     score = input('Enter score: ')
     s = float(score)
-    if s < 0.0 or s > 1.0:
-        print 'Bad score'
-    elif s >= 0.9:
-        print 'A'
-    elif s >= 0.8:
-        print 'B'
-    elif s >= 0.7:
-        print 'C'
-    elif s >= 0.6:
-        print 'D'
-    else:
-        print 'F'
+    print computegrade(s)
 except:
     print 'Bad score'
